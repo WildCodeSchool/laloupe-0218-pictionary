abf190de6114611a001e26f367af1c7effbfb22e
yohann <yohannmartinez34@gmail.com>
2018-03-29 16:05:23 +0200

    push homepage to dev

---
+++ src/app/homepage/homepage.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
+import{ Observable } from 'rxjs/Observable';
+import * as jQuery from 'jquery';
+
+
+@Component({
+  selector: 'app-homepage',
+  templateUrl: './homepage.component.html',
+  styleUrls: ['./homepage.component.css']
+})
+export class HomepageComponent implements OnInit {
+
+  title = "pictionary";
+  constructor(public afAuth: AngularFireAuth) {
+  }
+  login() {
+    this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+  }
+  logout() {
+    this.afAuth.auth.signOut();
+  }
+
+  ngOnInit() {
+    $(document).ready(function(){
+      $('.buttonnext').click(function(){
+        $('.contlogin').css({"transform" : "translate(0%,0%)"});
+      });
+      $('.buttongohome').click(function(){
+        $('.contlogin').css({"transform" : "translate(0%,-100%)"});
+      });
+    })
+  }
+
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,16 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBaRfYJ5CZGPWhGwK42FlUrXOAkTcU8vSM",
+    authDomain: "angularauth-62a23.firebaseapp.com",
+    databaseURL: "https://angularauth-62a23.firebaseio.com",
+    projectId: "angularauth-62a23",
+    storageBucket: "angularauth-62a23.appspot.com",
+    messagingSenderId: "838191329069"
+  }
+};
