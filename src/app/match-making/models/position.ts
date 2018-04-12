9f5612e7a8b2633f235ab123fadcb6649b1d51fa
MaximeSaliou72 <maximesaliou72@gmail.com>
2018-04-12 14:11:09 +0200

    Correction space

--- src/app/match-making/models/position.ts
+++ src/app/match-making/models/position.ts
@@ -1,11 +1,10 @@
 export class Position {
-    origin: {
-        x: number;
-        y: number;    
-    };
-     dest: {
-        x: number;
-        y: number;
-    };
-  }
-  
+  origin: {
+    x: number;
+    y: number;
+  };
+  dest: {
+    x: number;
+    y: number;
+  };
+}
