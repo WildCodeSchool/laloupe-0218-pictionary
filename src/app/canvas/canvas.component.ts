09a41a39a7b0b7e379a0231cca3dc2117e217480
BertrandBurel <legionxful@gmail.com>
2018-04-13 13:06:41 +0200

    Resolution bug

--- src/app/canvas/canvas.component.ts
+++ src/app/canvas/canvas.component.ts
@@ -1,6 +1,6 @@
 import {
   Component, Input, ElementRef, AfterViewInit, ViewChild,
-} from '@angular/core';
+  } from '@angular/core';
 
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireStorageModule } from 'angularfire2/storage';
@@ -21,8 +21,8 @@ import { Title } from '@angular/platform-browser';
 import { timer } from 'rxjs/observable/timer';
 import 'rxjs/add/observable/interval';
 
+// tslint:disable-next-line:no-duplicate-imports
 import { Router } from '@angular/router';
-import { router } from '../app.routes';
 
 @Component({
   selector: 'app-canvas',
@@ -31,7 +31,7 @@ import { router } from '../app.routes';
 })
 
 
-export class CanvasComponent implements AfterViewInit {
+  export class CanvasComponent implements AfterViewInit {
   room: Room;
   lines;
 
@@ -43,50 +43,40 @@ export class CanvasComponent implements AfterViewInit {
   @Input() public height = 400;
 
   constructor(private authService: AuthService,
-    private route: ActivatedRoute,
-    private router: Router,
-    private db: AngularFirestore) { }
+              private route: ActivatedRoute,
+              private db: AngularFirestore) { }
 
   ngOnInit() {
     this.lines = [];
     let index = 0;
-    /*   const timer = 60;
+  /* const timer = 60;
 
-      if (Object.keys(this.room.players).length === 2) {
-        Observable.interval(1000)
-        .take(61)
-        .subscribe((n) => {
-          console.log(timer - n);
-          if (n === timer) {
-            console.log('perdue');
-          }
-        });
-      } */
+  if (Object.keys(this.room.players).length === 2) {
+  Observable.interval(1000)
+  .take(61)
+  .subscribe((n) => {
+  console.log(timer - n);
+  if (n === timer) {
+  console.log('perdue');
+  }
+  });
+  } */
 
     this.roomId = this.route.snapshot.paramMap.get('id');
     this.db
-      .doc<Room>('rooms/' + this.roomId)
-      .valueChanges()
-      .subscribe((room) => {
-        this.room = room;
-        console.log(this.me);
-        console.log(this.opponent);
-        if (this.me.win) {
-          alert('Congratulations ! You find the word !');
-          this.router.navigate(['']);
-        } else if (this.opponent.win) {
-          alert('Congratulations ! the other player has find the word !');
-          this.router.navigate(['']);
-        }
+  .doc<Room>('rooms/' + this.roomId)
+  .valueChanges()
+  .subscribe((room) => {
+    this.room = room;
 
-        while (this.room.canvas && index < this.room.canvas.length) {
-          const element = this.room.canvas[index];
-          this.lines.push({ origin: element.origin, dest: element.dest });
-          this.drawOnCanvas(element.origin, element.dest);
-          index += 1;
-        }
-        console.log(this.room);
-      });
+    while (this.room.canvas && index < this.room.canvas.length) {
+      const element = this.room.canvas[index];
+      this.lines.push({ origin: element.origin, dest: element.dest });
+      this.drawOnCanvas(element.origin, element.dest);
+      index += 1;
+    }
+    console.log(this.room);
+  });
   }
 
   public ngAfterViewInit() {
@@ -105,32 +95,21 @@ export class CanvasComponent implements AfterViewInit {
     this.roomId = this.route.snapshot.paramMap.get('id');
   }
 
-  get me() {
-    if ((this.room.players[0].id === this.authService.authId)) {
-      return this.room.players[0];
-    }
-    return this.room.players[1];
-  }
+  checkWord(param) {
+    const inputValue = (<HTMLInputElement>document.getElementById('mot')).value;
 
-  get opponent() {
-    if ((this.room.players[0].id === this.authService.authId)) {
-      return this.room.players[1];
-    }
-    return this.room.players[0];
-  }
+    if (inputValue === this.room.randomWord) {
+      alert('you win');
 
-  checkWord(param){
-    let inputValue = (<HTMLInputElement>document.getElementById('mot')).value;
+    } else {
+      alert('try again');
 
-    if(inputValue === this.room.randomWord) {
-      this.me.win = true;
-      this.updateRoom();
     }
   }
 
   isMyTurn(): boolean {
     if ((this.room.players[0].id === this.authService.authId && this.room.turn === 0) ||
-      (this.room.players[1].id === this.authService.authId && this.room.turn === 1)) {
+  (this.room.players[1].id === this.authService.authId && this.room.turn === 1)) {
       return true;
     }
     return false;
@@ -139,42 +118,42 @@ export class CanvasComponent implements AfterViewInit {
   changeTurn() {
     this.room.turn = this.room.turn === 0 ? 1 : 0;
     this.db
-      .doc<Room>('rooms/' + this.roomId)
-      .set(this.room);
+  .doc<Room>('rooms/' + this.roomId)
+  .set(this.room);
   }
 
   private captureEvents(canvasEl: HTMLCanvasElement) {
     Observable
-      .fromEvent(canvasEl, 'mousedown')
-      .switchMap((e) => {
-        return Observable
-          .fromEvent(canvasEl, 'mousemove')
-          .takeUntil(Observable.fromEvent(canvasEl, 'mouseup'))
-          .pairwise();
-      })
-      .filter(() => {
-        return this.isMyTurn();
-      })
-      .subscribe((res: [MouseEvent, MouseEvent]) => {
-        const rect = canvasEl.getBoundingClientRect();
-        const prevPos = {
-          x: res[0].clientX - rect.left,
-          y: res[0].clientY - rect.top,
-        };
+  .fromEvent(canvasEl, 'mousedown')
+  .switchMap((e) => {
+    return Observable
+  .fromEvent(canvasEl, 'mousemove')
+  .takeUntil(Observable.fromEvent(canvasEl, 'mouseup'))
+  .pairwise();
+  })
+  .filter(() => {
+    return this.isMyTurn();
+  })
+  .subscribe((res: [MouseEvent, MouseEvent]) => {
+    const rect = canvasEl.getBoundingClientRect();
+    const prevPos = {
+      x: res[0].clientX - rect.left,
+      y: res[0].clientY - rect.top,
+    };
 
-        const currentPos = {
-          x: res[1].clientX - rect.left,
-          y: res[1].clientY - rect.top,
-        };
-        this.lines.push({ origin: prevPos, dest: currentPos });
-        this.drawOnCanvas(prevPos, currentPos);
-      });
+    const currentPos = {
+      x: res[1].clientX - rect.left,
+      y: res[1].clientY - rect.top,
+    };
+    this.lines.push({ origin: prevPos, dest: currentPos });
+    this.drawOnCanvas(prevPos, currentPos);
+  });
     Observable
-      .fromEvent(canvasEl, 'mouseup')
-      .subscribe((res: MouseEvent) => {
-        this.room.canvas = this.lines;
-        this.updateRoom();
-      });
+  .fromEvent(canvasEl, 'mouseup')
+  .subscribe((res: MouseEvent) => {
+    this.room.canvas = this.lines;
+    this.updateRoom();
+  });
   }
 
   private drawOnCanvas(prevPos: { x: number, y: number }, currentPos: { x: number, y: number }) {
